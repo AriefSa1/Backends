9ad13b82bdcb31d5060b0888f75c757b9b4aee7d
AriefSa1 <arifinnos2312@gmail.com>
2024-01-24 19:44:24 +0700

    last-one.sasdsas

--- middleware/uploadPictureMiddleware.js
+++ middleware/uploadPictureMiddleware.js
@@ -3,7 +3,7 @@ import path from "path";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, '/tmp'); // Use the /tmp directory for storing temporary files in Lambda
+    cb(null, path.join(__dirname, "../uploads"));
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`);
